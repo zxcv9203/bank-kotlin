3d97665c44d5a15acf62de07f7fe76af4edb76ae
yongckim <zxcv9203@naver.com>
2025-04-02 19:27:22 +0900

    :sparkles: Json 직렬화 / 역직렬화를 위한 유틸 객체 추가

---
+++ src/main/kotlin/org/example/bankkotlin/common/util/JsonUtils.kt
@@ -0,0 +1,19 @@
+package org.example.bankkotlin.common.util
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.json.Json
+
+object JsonUtils {
+    private val json = Json {
+        ignoreUnknownKeys = true
+        prettyPrint = true
+    }
+
+    fun <T> encodeToJson(v: T, serializer: KSerializer<T>): String {
+        return json.encodeToString(serializer, v)
+    }
+
+    fun <T> decodeFromJson(v: String, serializer: KSerializer<T>): T {
+        return json.decodeFromString(serializer, v)
+    }
+}
