46573f939846a9837ded1b5ab684a13c0f93b235
yongckim <zxcv9203@naver.com>
2025-05-06 01:32:21 +0900

    :sparkles: 카프카 프로듀서 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,8 +32,10 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.github.f4b6a3:ulid-creator:5.2.3")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.kafka:spring-kafka")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
     runtimeOnly("com.mysql:mysql-connector-j")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
---
+++ src/main/kotlin/org/example/bankkotlin/common/config/KafkaProducerConfig.kt
@@ -0,0 +1,32 @@
+package org.example.bankkotlin.common.config
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+@Configuration
+class KafkaProducerConfig(
+    @Value("\${spring.kafka.bootstrap-servers}")
+    private val servers: String,
+) {
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, Any> {
+        val props = HashMap<String, Any>()
+        props[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = servers
+        props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+
+        return DefaultKafkaProducerFactory(props)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, Any> {
+        return KafkaTemplate(producerFactory())
+    }
+}
