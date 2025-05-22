50da17db08b0cc1d4861e5750e023e1c8ab01fe3
yongckim <zxcv9203@naver.com>
2025-05-22 23:23:04 +0900

    :sparkles: jwt 인증 성공 로직 추가

--- src/main/kotlin/org/example/bankkotlin/security/filter/JwtFilter.kt
+++ src/main/kotlin/org/example/bankkotlin/security/filter/JwtFilter.kt
@@ -5,8 +5,10 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.example.bankkotlin.auth.domain.JwtProvider
+import org.example.bankkotlin.common.exception.CustomException
 import org.example.bankkotlin.common.exception.ErrorCode
 import org.example.bankkotlin.common.model.Response
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
@@ -26,20 +28,40 @@ class JwtFilter(
         val requestURI = request.requestURI
 
         if (shouldPerformAuthentication(requestURI)) {
+            val authHeader = request.getHeader(HttpHeaders.AUTHORIZATION)
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                val token = authHeader.substring(7)
+                try {
+                    jwtProvider.verifyToken(token)
+                } catch (e: CustomException) {
+                    response.status = HttpStatus.UNAUTHORIZED.value()
+                    response.contentType = MediaType.APPLICATION_JSON_VALUE
 
-        } else {
-            response.status = HttpServletResponse.SC_UNAUTHORIZED
-            response.contentType = MediaType.APPLICATION_JSON_VALUE
+                    val message = e.message
+                    val errorResponse = Response.failed(
+                        code = HttpStatus.UNAUTHORIZED,
+                        message = message ?: ErrorCode.ACCESS_TOKEN_NEED.message,
+                        data = Unit
+                    )
+                    response.writer.write(ObjectMapper().writeValueAsString(errorResponse))
+                    response.writer.flush()
+                }
+            } else {
+                response.status = HttpServletResponse.SC_UNAUTHORIZED
+                response.contentType = MediaType.APPLICATION_JSON_VALUE
 
-            val errorResponse = Response.failed(
-                code = HttpStatus.UNAUTHORIZED,
-                message = ErrorCode.ACCESS_TOKEN_NEED.message,
-                data = Unit
-            )
-            response.writer.write(ObjectMapper().writeValueAsString(errorResponse))
-            response.writer.flush()
+                val errorResponse = Response.failed(
+                    code = HttpStatus.UNAUTHORIZED,
+                    message = ErrorCode.ACCESS_TOKEN_NEED.message,
+                    data = Unit
+                )
+                response.writer.write(ObjectMapper().writeValueAsString(errorResponse))
+                response.writer.flush()
+            }
         }
+        filterChain.doFilter(request, response)
     }
+
 
     private fun shouldPerformAuthentication(uri: String): Boolean {
         for (endPoint in JWT_AUTH_ENDPOINT) {
